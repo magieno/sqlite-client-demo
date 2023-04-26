b0af0e67279206d624cbad1b8db07b2a60aae3c7
Etienne Noel <etienne.noel@ima-tech.ca>
2023-04-25 20:50:27 -0700

    - Updated the variable name in the documentation.

--- examples/vanilla-js/src/index.ts
+++ examples/vanilla-js/src/index.ts
@@ -4,12 +4,12 @@ const bootstrap = async () => {
   const webSqliteWorkerPath = "scripts/web-sqlite-worker.js"; // Must correspond to the path in your final deployed build.
   const filename = "/test.sqlite3"; // This is the name of your database. It corresponds to the path in the OPFS.
 
-  const sqliteClient = new WebSqlite(filename, webSqliteWorkerPath)
-  await sqliteClient.init();
+  const webSqlite = new WebSqlite(filename, webSqliteWorkerPath)
+  await webSqlite.init();
 
-  await sqliteClient.executeSql("CREATE TABLE IF NOT EXISTS test(a,b)");
-  await sqliteClient.executeSql("INSERT INTO test VALUES(?, ?)", [6,7]);
-  const results = await sqliteClient.executeSql("SELECT * FROM test");
+  await webSqlite.executeSql("CREATE TABLE IF NOT EXISTS test(a,b)");
+  await webSqlite.executeSql("INSERT INTO test VALUES(?, ?)", [6,7]);
+  const results = await webSqlite.executeSql("SELECT * FROM test");
   console.log("Results:", results);
 }
 
