ff1e326fb99f314037bc89aa54356d695ef2d498
Thorsten Wolf <thorsten.wolf@neptune-software.com>
2024-03-14 12:03:18 +0100

    bump up versions, update Readme & clientTypes

--- examples/vanilla-js/src/index.ts
+++ examples/vanilla-js/src/index.ts
@@ -1,29 +1,11 @@
-import { SqliteClientFactory, SqliteClient } from "@magieno/sqlite-client";
-import { SqliteClientBase } from "@magieno/sqlite-client/dist/esm/clients/sqlite-client-base"; 
-import { ExecuteSqlReturnValue } from "@magieno/sqlite-client/dist/esm/enums/execute-sql-return-value.enum";
-import { ExecuteSqlRowMode } from "@magieno/sqlite-client/dist/esm/enums/execute-sql-row-mode.enum";
-import { SqliteClientType } from "@magieno/sqlite-client/dist/esm/enums/sqlite-client-type.enum";
+import { SqliteClient } from "@magieno/sqlite-client"; 
+import { SqliteClientTypeEnum } from "@magieno/sqlite-client/dist/esm/enums/sqlite-client-type.enum";
 
-window.onload = () => {
-  // Initialize event listeners once the window has loaded 
-  document.getElementById('legacyClient')?.addEventListener('change', toggleLegacyCheckbox);
-
-}
-
-function toggleLegacyCheckbox(): void {
-  const legacyClient = document.getElementById('legacyClient') as HTMLInputElement;
-  const clientTypeSelect = document.getElementById('clientType') as HTMLSelectElement;
-
-  clientTypeSelect.disabled = legacyClient.checked;
-  clientTypeSelect.value = 'OPFS';
-}
 
 function runSqlite(): void {
   const clientTypeSelect = document.getElementById('clientType') as HTMLSelectElement;
 
-  const legacyClient = document.getElementById('legacyClient') as HTMLInputElement;
-
-  run(clientTypeSelect.value as SqliteClientType, legacyClient.checked);
+  run(clientTypeSelect.value as SqliteClientTypeEnum);
 }
 
 // Adapt the HTML button onclick to directly call submitForm() without needing an inline event handler
@@ -31,33 +13,31 @@ document.querySelector('button')?.addEventListener('click', runSqlite);
 
 const sqliteClients = {};
 
-const run = async (clientType: SqliteClientType, legacyClient?: boolean) => {
+const run = async (clientType: SqliteClientTypeEnum) => {
   const webSqliteWorkerPath = "scripts/sqlite-worker.mjs"; // Must correspond to the path in your final deployed build.
   const filename = "/test.sqlite3"; // This is the name of your database. It corresponds to the path in the OPFS.
 
-  let webSqlite: SqliteClientBase;
+  let webSqlite: SqliteClient;
   if (sqliteClients[clientType]) {
     webSqlite = sqliteClients[clientType];
   } else {
-    if (legacyClient) {
-      webSqlite = new SqliteClient(filename, "c", webSqliteWorkerPath)
-    } else {
-      webSqlite = SqliteClientFactory.getInstance({
-        clientType: clientType,
-        options: {
-          filename: filename,
-          flags: "c",
-          sqliteWorkerPath: webSqliteWorkerPath,
-        }
-      });
-    }
+
+    webSqlite = new SqliteClient(
+      {
+        type: clientType,
+        filename: filename,
+        sqliteWorkerPath: webSqliteWorkerPath,
+        flags: "c",
+      }
+    );
+
     await webSqlite.init();
     sqliteClients[clientType] = webSqlite;
   }
 
-  await webSqlite.executeSql("CREATE TABLE IF NOT EXISTS test(a,b)");
+  await webSqlite.executeSql("CREATE TABLE IF NOT EXISTS test(a,b)", []);
   await webSqlite.executeSql("INSERT INTO test VALUES(?, ?)", [6, 7]);
-  const results = await webSqlite.executeSql("SELECT * FROM test", undefined, ExecuteSqlReturnValue.resultRows, ExecuteSqlRowMode.object);
+  const results = await webSqlite.executeSql("SELECT * FROM test", []);
   console.log("Results:", results);
 
 }
